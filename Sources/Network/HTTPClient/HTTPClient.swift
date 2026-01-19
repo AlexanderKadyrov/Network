75e1e54e750a7b1381999820aa5f2d13473704e2
Alexander <alexander-kad@yandex.ru>
2026-01-19 17:50:04 +0500

    add new methods (#2)

--- Sources/Network/HTTPClient/HTTPClient.swift
+++ Sources/Network/HTTPClient/HTTPClient.swift
@@ -1,6 +1,8 @@
 import Foundation
+import Combine
 import DI
 
 public protocol HTTPClient {
-    func fetchRequest(url: URL) async throws -> Data
+    func fetch(url: URL) -> AnyPublisher<Data, Error>
+    func fetch<T: Decodable>(url: URL, type: T.Type) -> AnyPublisher<T, Error>
 }
